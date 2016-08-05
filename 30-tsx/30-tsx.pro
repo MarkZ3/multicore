d703e88e13950a0b52e27ff2e9b827625a7a68c7
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-08-05 11:08:23 -0400

    Add some statistics
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- 30-tsx/30-tsx.pro
+++ 30-tsx/30-tsx.pro
@@ -4,12 +4,14 @@ QT -= gui
 TARGET = 30-tsx
 CONFIG += console
 CONFIG -= app_bundle
-CONFIG += c++14
+CONFIG += c++11
 
 # force to use RTM instructions
 QMAKE_CXXFLAGS += -mrtm
 
 TEMPLATE = app
+
+LIBS += -ltbb
 
 SOURCES += main.cpp
 
--- 30-tsx/main.cpp
+++ 30-tsx/main.cpp
@@ -1,5 +1,6 @@
 #include <QDebug>
 #include <QVector>
+#include <QElapsedTimer>
 
 #include <mutex>
 #include <random>
@@ -7,6 +8,7 @@
 #include <thread>
 #include <chrono>
 #include <urcu/uatomic.h>
+#include <tbb/tbb.h>
 
 #include "tsx-cpuid.h"
 
@@ -42,16 +44,26 @@ public:
 class TransactionScope {
 public:
     TransactionScope(SpinLock *lock) : m_lock(lock), m_code(0) {
-        m_code = _xbegin();
-        if (m_code == _XBEGIN_STARTED) {
-            if (!m_lock->isLocked()) {
-                return;
-            } else {
-                _xabort(1);
+        int retries = 0;
+        static int max_retries = 10;
+        while(retries < max_retries)  {
+            m_code = _xbegin();
+            if (m_code == _XBEGIN_STARTED) {
+                if (!m_lock->isLocked()) {
+                    return;
+                } else {
+                    _xabort(0xFF); // explicit abort because lock is already locked
+                }
             }
-        } else {
-            m_lock->lock();
+            uatomic_inc(&m_abort);
+            if ((m_code & _XABORT_EXPLICIT) && (_XABORT_CODE(m_code) == 0xFF)
+                && !(m_code & _XABORT_NESTED)) {
+                while(m_lock->isLocked()) {
+                    _mm_pause();
+                }
+            }
         }
+        m_lock->lock();
     }
 
     ~TransactionScope() {
@@ -66,48 +78,98 @@ public:
         return m_code;
     }
 
+    static void reset() {
+        m_abort = 0;
+    }
+
     SpinLock *m_lock;
     uint m_code;
+    static uint m_abort;
 };
 
-volatile int run;
+uint TransactionScope::m_abort = 0;
 
-void worker(SpinLock *lock, QVector<int> *data, int amount)
+double elapsed(std::function<void ()> func)
 {
-    int *buf = data->data();
-    uint size = data->size();
-    uint i = 0;
-    while(run) {
-        TransactionScope scope(lock);
-        buf[i++ % size] += amount;
-    }
+    QElapsedTimer timer;
+    timer.start();
+    func();
+    return timer.nsecsElapsed() / 1000000000.0f;
 }
+
+void print_result(QString name, double reference, double actual)
+{
+    qDebug() << QString("%1 %2s (%3x)")
+                .arg(name, -12, QLatin1Char(' '))
+                .arg(actual, 0, 'f', 6)
+                .arg(reference / actual, 0, 'f', 2);
+}
+
+void do_transaction(QVector<int> &accounts, QVector<int> &rnd, int i)
+{
+    int amount = 100;
+    int from = rnd[i % rnd.size()];
+    int to = rnd[(i + 1) % rnd.size()];
+    accounts[from] -= amount;
+    accounts[to] += amount;
+}
+
+void do_transaction2(int *accounts, int from, int to)
+{
+    int amount = 100;
+    accounts[from] -= amount;
+    accounts[to] += amount;
+}
+
+typedef tbb::concurrent_vector<int> ConcurrentVector;
 
 int main()
 {
-    SpinLock lock;
-    int n = 1E3;
-    QVector<int> data(n);
-
     if (!cpu_has_rtm()) {
         qDebug() << "rtm not supported";
         return -1;
     }
 
-    run = 1;
-    __sync_synchronize();
+    std::random_device rd;
+    std::mt19937 g(rd());
 
-    std::thread t1(worker, &lock, &data, -1);
-    std::thread t2(worker, &lock, &data, 1);
+    int iter = 1E7;
+    for (uint size = 1E3; size < 1E7; size *= 10) {
+    //for (uint size = 1E6; size <= 1E6; size *= 10) {
+        QVector<int> accounts(size);
+        QVector<int> rnd(size);
+        for (uint x = 0; x < size; x++) {
+            rnd[x] = x;
+        }
+        std::shuffle(rnd.begin(), rnd.end(), g);
 
-    std::chrono::milliseconds delay(1000);
-    std::this_thread::sleep_for(delay);
+        double serial = elapsed([&]() {
+            for (int i = 0; i < iter; i++) {
+                do_transaction(accounts, rnd, i);
+            }
+        });
 
-    run = 0;
-    __sync_synchronize();
+        SpinLock lock;
+        TransactionScope::reset();
+        double parallel_rtm = elapsed([&]() {
+            tbb::parallel_for(0, iter, [&](int &i) {
+                int amount = 100;
+                int from = rnd[i % rnd.size()];
+                int to = rnd[(i + 1) % rnd.size()];
+                int *data = accounts.data();
+                TransactionScope scope(&lock);
+                data[from] -= amount;
+                data[to] += amount;
+            });
+        });
 
-    t1.join();
-    t2.join();
+        qDebug() << "size=" << size;
+        print_result("serial", serial, serial);
+        print_result("parallel rtm", serial, parallel_rtm);
+        qDebug() << "abort" << TransactionScope::m_abort;
+        qDebug() << "abort rate" << QString("%1").arg(TransactionScope::m_abort / (double) iter, 0, 'f', 2);
+    }
+
     return 0;
 }
 
